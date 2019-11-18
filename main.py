0769b0d9852ee996840a78bed4d3000078446d93
Andrew Tkachev <sim200347@gmail.com>
2019-11-18 19:58:03 +0300

    Improve area generation and matrix of fields

--- main.py
+++ main.py
@@ -1,82 +1,110 @@
 import sys
 import pygame
-import random
+from random import randint
 
+class Area:
+    def __init__(self):
+        self.area_data = []
+        self.width = 1550
+        self.height = 650
+        self.size_block = 50
+        self.rgb = [
+            (150, 0, 0),
+            (0, 150, 0),
+            (0, 0, 150)
+        ]
+        self.area_data = []
+        self.create_area()
 
-class BomberMan:
-    filename = 'bomberman.png'
+    def create_area(self):
+        for i in range(13):
+            self.area_data.append([])
 
-    def __init__(self, x=10, y=90):
-        self.image = pygame.image.load(self.filename)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.shift_x = 0
-        self.shift_y = 0
+        for i in range(13):
+            for j in range(31):
+                self.area_data[i].append(1)
+        for i in range(13):
+            self.area_data[i][0] = 0
+            self.area_data[i][30] = 0
+        for i in range(31):
+            self.area_data[0][i] = 0
+            self.area_data[12][i] = 0
 
-    def beginning_position(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
+        for i in range(2, 12, 2):
+            for j in range(2, 30, 2):
+                self.area_data[i][j] = 0
 
-    def process_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                self.shift_x = -10
-            elif event.key == pygame.K_d:
-                self.shift_x = 10
-            elif event.key == pygame.K_w:
-                self.shift_y = 10
-            elif event.key == pygame.K_s:
-                self.shift_y = -10
-
-    def process_logic(self, width, height):
-        if self.rect.left < 0 or self.rect.right > width:
-            self.shift_x = 0
-        elif self.rect.bottom < 0 or self.rect.top > height:
-            self.shift_y = 0
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > width:
-            self.rect.right = width
-        if self.rect.bottom < 0:
-            self.rect.bottom = 0
-        if self.rect.top > height:
-            self.rect.top = height
-
-    def move(self):
-        self.rect.x += self.shift_x
-        self.rect.y += self.shift_y
+        count = 0
+        n = randint(50, 70)
+        for i in range(13):
+            for j in range(31):
+                if i % 2 == 1 and j % 2 == 1 and i != 1 and i != 2 and j != 1 and j != 2 and count <= n:
+                    if randint(0, 1):
+                        self.area_data[i][j] = 2
+                        count += 1
 
     def process_draw(self, screen):
-        screen.blit(self.image, self.rect)
+        # for h in range(75, self.height + 75, self.size_block):
+        #     for w in range(0, self.width, self.size_block):
+        #         pygame.draw.rect(screen, self.rgb[(w + h) % 3], (w, h, self.size_block, self.size_block))
+        for i in range(13):
+            print(self.area_data[i])
+
+    def process_logic(self):
+        pass
+
+
+class Game:
+    def __init__(self, width=800, height=625):
+        self.width = width
+        self.height = height
+        self.size = (width, height)
+        self.game_over = False
+        self.down_left = False
+        self.screen = pygame.display.set_mode(self.size)
+        pygame.init()
+        self.create_objects()
+
+    def process_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game_over = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == 275 or event.key == 100:
+                    self.down_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == 275 or event.key == 100:
+                    self.down_left = False
+                print(event.key)
+
+    def create_objects(self):
+        self.area = Area()
+        self.area.process_draw(self.screen)
+
+    def main_loop(self):
+        while not self.game_over:
+            self.process_event()
+            self.screen.fill((75, 100, 150))
+
+
+            # if self.down_left and x < size_screen[0] - 25 - 12.5:
+            #     x += 2.5
+
+            # pygame.draw.rect(screen, (0, 0, 0), (x + 12.5, 75 + 12.5, 25, 25))
+            # x += 10
+            # pygame.draw.rect(screen, (0, 150, 0), (50, 50, 50, 50))
+            # pygame.draw.rect(screen, (0, 0, 150), (0, 100, 50, 50))
+
+            pygame.display.flip()
+            pygame.time.wait(10)
+        sys.exit()
 
 
 def main():
-    rgb = [
-        (150, 0, 0),
-        (0, 150, 0),
-        (0, 0, 150)
-    ]
-    size_block = 50
-    size_screen = (800, 625)
-    pygame.init()
-    screen = pygame.display.set_mode(size_screen)
-    x = 0
-    game_over = False
-    bomberman = BomberMan(50, 125)
-    while not game_over:
-        for event in pygame.event.get():
-            bomberman.process_event(event)
-            if event.type == pygame.QUIT:
-                game_over = True
-        screen.fill((75, 100, 150))
-        for h in range(75, size_screen[1] - 25, size_block):
-            for i in range(0, size_screen[0], size_block):
-                pygame.draw.rect(screen, rgb[(i + h) % 3], (i, h, size_block, size_block))
-        bomberman.process_draw(screen)
-        pygame.display.flip()
-        pygame.time.wait(10)
-    sys.exit()
+    # x = 0
+
+    game = Game(800, 725)
+    game.main_loop()
 
 
 if __name__ == '__main__':
