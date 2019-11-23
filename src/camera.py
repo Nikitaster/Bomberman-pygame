efafe117857f6e78db2a055a95207092e1471e8c
Andrew Tkachev <sim200347@gmail.com>
2019-11-23 22:09:04 +0300

    Fix bomber speed movement

--- src/camera.py
+++ src/camera.py
@@ -10,7 +10,7 @@ class Camera:
         self.state = self.camera_func(self.state, target.rect)
 
     def apply(self, target, speed):
-        return target.rect.move((self.state.x * speed / 4, self.state.y))
+        return target.rect.move((self.state.x * speed / 5, self.state.y))
 
 
 def camera_func(camera, target_rect):
@@ -19,7 +19,7 @@ def camera_func(camera, target_rect):
     l, t = -l + 800 / 2, -t + 650 / 2
 
     l = min(0, l)  # Не движемся дальше левой границы
-    l = max(-(camera.width - 950), l)       # Не движемся дальше правой границы
+    l = max(-(camera.width - 800), l)       # Не движемся дальше правой границы
     t = max(-(camera.height-650), t)        # Не движемся дальше нижней границы
     t = min(0, t)                           # Не движемся дальше верхней границы
     return Rect(l, t, w, h)
