3b9a468f704140ae34a71c85501764b4c4ea740c
Andrew Tkachev <sim200347@gmail.com>
2019-11-23 12:08:51 +0300

    Delete dublicates, that was transported into src/camera.py

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 import sys
 import pygame
 from random import randint
-from pygame.rect import Rect
-
-
-class Camera:
-    def __init__(self, camera_func, width, height):
-        self.camera_func = camera_func
-        self.state = Rect(0, 0, width, height)
-
-    def update(self, target):
-        self.state = self.camera_func(self.state, target.rect)
-
-    def apply(self, target, speed):
-        return target.rect.move((self.state.x * speed / 4, self.state.y))
-
-
-def camera_func(camera, target_rect):
-    l, t, _, _ = target_rect
-    _, _, w, h = camera
-    l, t = -l + 800 / 2, -t + 650 / 2
-
-    l = min(0, l)  # Не движемся дальше левой границы
-    l = max(-(camera.width - 950), l)       # Не движемся дальше правой границы
-    t = max(-(camera.height-650), t)        # Не движемся дальше нижней границы
-    t = min(0, t)                           # Не движемся дальше верхней границы
-    return Rect(l, t, w, h)
+from src.camera import Camera, camera_func
 
 
 class Cell:
