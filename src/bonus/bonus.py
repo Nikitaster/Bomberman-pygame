784dd4747cb4f1d9384468844bc9ec9096ff2e4c
Nikita Gudkov <nikitaster2001@gmail.com>
2019-11-30 20:56:11 +0300

    prepare for creating bonus classes

--- src/bonus/bonus.py
+++ src/bonus/bonus.py
@@ -2,8 +2,14 @@ from src.blocks.cell import Cell
 
 
 class Bonus(Cell):
-    image = None # в дочерних: image = pygame.image.load("PATH")
+    image = None  # в дочерних: image = pygame.image.load("PATH")
 
     def __init__(self, x=0, y=75):
         super().__init__(x, y)
-        self.type = "Bonus"
+        self.type = "Bonus"  # для каждого бонуса моенять type
+        self.status = 'Hidden'  # Open, Taken
+
+    def process_logic(self):
+        # тут должна происходить логика бомбы в зависимости от ее текущего статуса
+        # с последующей сменой этого статуса на следущий.
+        pass
