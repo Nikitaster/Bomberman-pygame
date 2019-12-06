dd657f1ff387bcea49b82d633b1303e2579f50af
Andrew Tkachev <sim200347@gmail.com>
2019-12-06 20:23:26 +0300

    Remove extra blanklines

--- src/screens/menu.py
+++ src/screens/menu.py
@@ -1,5 +1,4 @@
 import os
-
 import pygame
 
 
@@ -12,14 +11,11 @@ class Menu:
     green = (0, 255, 0)
     blue = (0, 0, 255)
     yellow = (255, 255, 0)
-
     # Game Fonts
     font = "fonts/pixel.ttf"
-
     # Game Framerate
     clock = pygame.time.Clock()
     FPS = 30
-
     # Center the Game Application
     os.environ['SDL_VIDEO_CENTERED'] = '1'
 
@@ -31,15 +27,14 @@ class Menu:
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
 
     # Text Renderer
-    def text_format(self, message, textFont, textSize, textColor):
-        newFont = pygame.font.Font(textFont, textSize)
-        newText = newFont.render(message, 0, textColor)
-        return newText
+    def text_format(self, message, textfont, textsize, textcolor):
+        newfont = pygame.font.Font(textfont, textsize)
+        newtext = newfont.render(message, 0, textcolor)
+        return newtext
 
     def main_menu(self):
         menu = True
         selected = "start"
-
         while menu:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -52,13 +47,9 @@ class Menu:
                         selected = "quit"
                     if event.key == pygame.K_RETURN:
                         if selected == "start":
-                            # exec(open('./run.py').read())
                             return True
                         if selected == "quit":
-                            # pygame.quit()
-                            # quit()
                             return False
-
             # Main Menu UI
             self.screen.fill(self.black)
             title = self.text_format("BOMBERMAN", self.font, 90, self.yellow)
@@ -70,11 +61,9 @@ class Menu:
                 text_quit = self.text_format("QUIT", self.font, 55, self.white)
             else:
                 text_quit = self.text_format("QUIT", self.font, 45, self.red)
-
             title_rect = title.get_rect()
             start_rect = text_start.get_rect()
             quit_rect = text_quit.get_rect()
-
             self.screen.blit(title, (self.screen_width / 2 - (title_rect[2] / 2), 80))
             self.screen.blit(text_start, (self.screen_width / 2 - (start_rect[2] / 2), 300))
             self.screen.blit(text_quit, (self.screen_width / 2 - (quit_rect[2] / 2), 370))
